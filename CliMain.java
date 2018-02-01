fd1ebcbcb29db2adf0792d3caf910ccce93638b0
Hamza Khanane <khananehamza@gmail.com>
2018-02-01 17:55:15 -0500

    added the methods
    implemented the getsearch(), getrandom() and getindex() methods

--- CliMain.java
+++ CliMain.java
@@ -2,6 +2,9 @@ package quotes;
 import java.util.ArrayList;
 
 public class CliMain {
+
+
+  //private Random randomGen;
   public static void main(String[] args) {
     // Parse command line options
     for (String arg : args) {
@@ -10,14 +13,38 @@ public class CliMain {
   }
 
   public static getIndex(int index) {
+    Quote q = this.qoutelist.get(i);
+    System.out.println(q);
+
+
+
     // TODO - return quote with index `index`
   }
 
-  public static getSearch(String terms) {
+  public static getSearch(String terms, int mode) {
+
+
+    QuoteList returnQuote = this.qoutelist.search(terms,mode);
+    for(int i=0; i<returnQuote.getSize(); i++)
+    {
+      System.out.println(returnQuote.get(i));
+
+    }
+
+
     // TODO - return quotes matching search
   }
 
-  public static gerRandom() {
+  public static getRandom() {
+
+    Quote q = this.qoutelistqgetRandomQuote();
+
+
+//    Random r = new Random();
+//    int rando = r.nextInt(this.qoutelist.size()-1);
+//    Quote q = this.qoutelist.get(rand);
+//    System.out.println(q);
+
     // TODO - return a random quote
   }
 }
