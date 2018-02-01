4ced9b0b5432e12664ae597627781f738a21db0d
Andriy Zasypkin <AndriyZasypkin@gmail.com>
2018-02-01 17:25:08 -0500

    added skeleton

--- CliMain.java
+++ CliMain.java
@@ -3,6 +3,21 @@ import java.util.ArrayList;
 
 public class CliMain {
   public static void main(String[] args) {
+    // Parse command line options
+    for (String arg : args) {
+      // parse arguments
+    }
+  }
 
+  public static getIndex(int index) {
+    // TODO - return quote with index `index`
+  }
+
+  public static getSearch(String terms) {
+    // TODO - return quotes matching search
+  }
+
+  public static gerRandom() {
+    // TODO - return a random quote
   }
 }
--- README.md
+++ README.md
@@ -8,3 +8,8 @@
 `java CliMain` get random quote
 `java CliMain -i|--index=<quote number>` get quote with index
 `java CliMain [<search terms>] [--author|-a] [--quote|-q] [--both|-b]` search
+
+# Documentation of Changes
+1. removed the `quoteserve.java` - no methods from this file were used
+2. removed deploy file, changing it to a makefile for easier compilation
+3. Created `CliMain.java` file for a main method to use for the command line
