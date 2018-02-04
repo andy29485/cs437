c498b8decd4aa2eeb23c93b3bb2c6cf02bfb6e88
Andriy Zasypkin <AndriyZasypkin@gmail.com>
2018-02-04 15:12:43 -0500

    added commnets/split main

--- CliMain.java
+++ CliMain.java
@@ -3,66 +3,81 @@ import java.util.ArrayList;
 
 public class CliMain {
   private static QuoteList quoteList;
+  public enum Action {INDEX, SEARCH, RANDOM, HELP};
 
   //private Random randomGen;
   public static void main(String[] args) {
-    // Initiallize the quote list
-    String quotefile = "quotes/quotes.xml";
-    ArrayList<Integer> ignore = new ArrayList<Integer>();
-    int type = 0;
-    int mode = QuoteList.SearchBothVal;
+    // Parse arguments
+    Options options = parseArgs(args);
 
+    // Load quotes
+    QuoteSaxParser qParser = new QuoteSaxParser(options.quotefile);
+    CliMain.quoteList      = qParser.getQuoteList();
+
+    switch(options.action) {
+      case RANDOM:
+        getRandom();
+        break;
+      case INDEX:
+        getIndex(options.param);
+        break;
+      case SEARCH:
+        getSearch(options.searchStr, options.param);
+        break;
+      case HELP:
+      default:
+        printHelp();
+    }
+
+    System.exit(0);
+  }
+
+  public static Options parseArgs(String[] args) {
+    ArrayList<Integer> ignore = new ArrayList<Integer>();
+    Options options = new Options();
     String arg;
+
     // Parse command line options
     for (int i=0; i<args.length; ++i) {
       arg = args[i];
       if(arg.startsWith("-f") || arg.startsWith("--file")) {
-        ignore.add(i);
+        ignore.add(i); // ignore this and next arg when joining search terms
         ignore.add(i+1);
-        quotefile = args[++i];
+        options.quotefile = args[++i]; // set filepath to next arg
       }
       else if(arg.startsWith("-i") || arg.startsWith("--index")) {
         ignore.add(i);
         ignore.add(i+1);
-        mode = Integer.valueOf(args[++i]);
-        type = 1;
+        options.action = Action.INDEX;
+        options.param  = Integer.valueOf(args[++i]); // set index to next arg
       }
       else if(arg.startsWith("-a") || arg.startsWith("--author")) {
         ignore.add(i);
-        type = 2;
-        mode = QuoteList.SearchAuthorVal;
+        options.action = Action.SEARCH;
+        options.param  = QuoteList.SearchAuthorVal;
       }
       else if(arg.startsWith("-q") || arg.startsWith("--quote")) {
         ignore.add(i);
-        type = 2;
-        mode = QuoteList.SearchTextVal;
+        options.action = Action.SEARCH;
+        options.param  = QuoteList.SearchTextVal;
       }
       else if(arg.startsWith("-b") || arg.startsWith("--both")) {
         ignore.add(i);
-        type = 2;
-        mode = QuoteList.SearchBothVal;
+        options.action = Action.SEARCH;
+        options.param  = QuoteList.SearchBothVal;
       }
-      else {
-        type = 2;
+      else if(arg.startsWith("-h") || arg.startsWith("--help")) {
+        options.action = Action.HELP;
+      }
+      else { // unmatched argument, must be a search term
+        options.action = Action.SEARCH;
       }
     }
 
-    QuoteSaxParser qParser = new QuoteSaxParser (quotefile);
-    CliMain.quoteList = qParser.getQuoteList();
+    // save unused arguments as the search string
+    options.searchStr = join(args, ignore);
 
-    switch(type) {
-      case 0:
-        getRandom();
-        break;
-      case 1:
-        getIndex(mode);
-        break;
-      case 2:
-        getSearch(join(args, ignore), mode);
-        break;
-    }
-
-    System.exit(0);
+    return options;
   }
 
   public static String join(String[] strings, ArrayList<Integer> ignore_indices) {
@@ -71,7 +86,9 @@ public class CliMain {
 
     // add strings to list except the ones to ignore
     for(int i=0; i<strings.length; i++) {
-      if(ignore_indices.indexOf(i) == -1) strList.add(strings[i]);
+      if(ignore_indices.indexOf(i) == -1) {
+        strList.add(strings[i]);
+      }
     }
 
     // return the strings joined by a space
@@ -85,7 +102,7 @@ public class CliMain {
   }
 
   public static void getSearch(String terms, int mode) {
-    QuoteList returnQuote = CliMain.quoteList.search(terms,mode);
+    QuoteList returnQuote = CliMain.quoteList.search(terms, mode);
     for(int i=0; i<returnQuote.getSize(); i++) {
       System.out.println(returnQuote.getQuote(i));
     }
@@ -94,5 +111,36 @@ public class CliMain {
   public static void getRandom() {
     Quote q = CliMain.quoteList.getRandomQuote();
     System.out.println(q);
+  }
+
+  public static void printHelp() {
+    System.out.println("Quotes program usage:");
+    System.out.println();
+    System.out.println("General arguments:");
+    System.out.println("  -h/--help          - this help page");
+    System.out.println("  -f/--file <path>   - specify path to quotes file");
+    System.out.println("  -i/--index <index> - print quote with given index");
+    System.out.println("Search arguments:");
+    System.out.println("  -a/--author        - search only author");
+    System.out.println("  -q/--quote         - search only quotes");
+    System.out.println("  -b/--both          - search both quote and author");
+    System.out.println();
+    System.out.println(" Default mode (no args) will return a random quote");
+    System.out.println(" When in search mode, all unmatched");
+    System.out.println("   args will be part of the search");
+  }
+
+  private static class Options {
+    public String quotefile; // str with file path to xml file w/ quotes
+    public String searchStr; // search string extracted from arguments
+    public Action action;    // type of action to perform
+    public int    param;     // param to pass to action
+
+    public Options() {
+      this.quotefile = "quotes/quotes.xml";
+      this.searchStr = "";
+      this.action    = Action.RANDOM;
+      this.param     = QuoteList.SearchBothVal;
+    }
   }
 }
