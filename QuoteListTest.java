d549f6c5a98ee88d54055eb27fb7f75cd4e66278
hamzakhanane <35496097+hamzakhanane@users.noreply.github.com>
2018-02-15 16:54:46 -0500

    Add files via upload

---
+++ QuoteListTest.java
@@ -0,0 +1,24 @@
+package quotes;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.*;
+
+public class QuoteListTest {
+  public static void main(String args[]){
+    org.junit.runner.JUnitCore.main("QuoteListTest");
+  }
+  
+  
+  @Test (timeout=1000) public void search_1 (){
+    String s = "Offut";
+    int mode = 0;
+    QuoteList q = QuoteList.search(s,mode);
+    assertEquals("hamza",q);
+    
+  }
+  
+  
+  
+  
+}
