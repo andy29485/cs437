6471954f2f6ed2cc9d61d545c525e250980bedeb
hamzakhanane <35496097+hamzakhanane@users.noreply.github.com>
2018-02-15 17:08:18 -0500

    Add files via upload
    got it to work

--- QuoteListTest.java
+++ QuoteListTest.java
@@ -1,8 +1,13 @@
 package quotes;
 
+
 import org.junit.*;
 import static org.junit.Assert.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Random;
+import java.io.*;
+import javax.xml.stream.*;
+
 
 public class QuoteListTest {
   public static void main(String args[]){
@@ -10,11 +15,14 @@ public class QuoteListTest {
   }
   
   
+  //QuoteList qlist = new QuoteList();
+  
   @Test (timeout=1000) public void search_1 (){
     String s = "Offut";
     int mode = 0;
-    QuoteList q = QuoteList.search(s,mode);
-    assertEquals("hamza",q);
+    QuoteList qlist = new QuoteList();
+    qlist.search(s,mode);
+    assertEquals("hamza", qlist.search(s,mode));
     
   }
   
