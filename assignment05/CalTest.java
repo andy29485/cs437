67a7e6ffcc0ff6ffa5ab478cb98d0a77bc4dc829
hamzakhanane <35496097+hamzakhanane@users.noreply.github.com>
2018-02-22 17:23:46 -0500

    Add files via upload
    new testcases

--- assignment05/CalTest.java
+++ assignment05/CalTest.java
@@ -11,54 +11,68 @@ public class CalTest {
 
   @Before
   public void setup() {
-    // TODO
+    
   }
 
-  @Test
-  public void testSameMonthDiffDay() {
-    int days_diff = Cal.cal(1, 3, 1, 12, 2018);
-    assertEquals("Same Month Different Days", days_diff, 9);
-  }
 
-  @Test
-  public void testSameMonthSameDay() {
-    int days_diff = Cal.cal(1, 8, 1, 8, 2018);
-    assertEquals("Same Month Same Day", days_diff, 0);
-  }
 
-  @Test
-  public void testFebMarchLeapYear() {
-    int days_diff = Cal.cal(2, 20, 3, 1, 2016);
-    assertEquals("Check End of Feb Leap Year (2016)", days_diff, 10);
-  }
 
-  @Test
-  public void testFebMarchLeapYear2000() {
-    int days_diff = Cal.cal(2, 20, 3, 1, 2000);
-    assertEquals("Check End of Feb Leap Year (2000)", days_diff, 10);
-  }
-
-  @Test
-  public void testFebMarchNotLeapYear() {
-    int days_diff = Cal.cal(2, 20, 3, 1, 2018);
-    assertEquals("Check End of Feb NOT Leap Year (2018)", days_diff, 9);
-  }
-
-  @Test
-  public void testFebMarchNotLeapYear2100() {
-    int days_diff = Cal.cal(2, 20, 3, 1, 2017);
-    assertEquals("Check End of Feb NOT Leap Year (2100)", days_diff, 9);
-  }
-
-  @Test
-    public void testEndDates() {
-    Cal c = new Cal();
-    int month1=1;
-    int day1=31;
-    int month2 = 3;
-    int day2 = 31;
-    int year = 2018;
-    int x = c.cal(month1,day1,month2,day2,year);
-    assertEquals(59,x);
-  }
+@Test
+  public void testEndDates() {
+  Cal c = new Cal();
+  int month1=1;
+  int day1=31;
+  int month2 = 4;
+  int day2 = 30;
+  int year = 2018;
+  int x = c.cal(month1,day1,month2,day2,year);
+  assertEquals(89,x); 
 }
+
+@Test
+  public void testDiff_Day_Diff_Mon() {
+  Cal c = new Cal();
+  int month1=11;
+  int day1=13;
+  int month2 = 12;
+  int day2 = 18;
+  int year = 2017;
+  int x = c.cal(month1,day1,month2,day2,year);
+  assertEquals(35,x); 
+}
+
+@Test
+  public void test_DiffDay_SameMonth() {
+  Cal c = new Cal();
+  int month1=12;
+  int day1=18;
+  int month2 = 12;
+  int day2 = 31;
+  int year = 2017;
+  int x = c.cal(month1,day1,month2,day2,year);
+  assertEquals(13,x); 
+}
+
+@Test
+  public void testSameDay_DiffMonth() {
+  Cal c = new Cal();
+  int month1=10;
+  int day1=31;
+  int month2 = 12;
+  int day2 = 31;
+  int year = 2018;
+  int x = c.cal(month1,day1,month2,day2,year);
+  assertEquals(61,x); 
+}
+
+
+
+
+
+  
+
+
+}
+  
+
+
