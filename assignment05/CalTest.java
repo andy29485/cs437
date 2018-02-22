10a8005f41183d5c838a2b2e76a3730589efbb28
Andriy Zasypkin <AndriyZasypkin@gmail.com>
2018-02-22 17:15:01 -0500

    added some tests to assign 05

--- assignment05/CalTest.java
+++ assignment05/CalTest.java
@@ -13,4 +13,40 @@ public class CalTest {
   public void setup() {
     // TODO
   }
+
+  @Test
+  public void testSameMonthDiffDay() {
+    int days_diff = Cal.cal(1, 3, 1, 12, 2018);
+    assertEquals("Same Month Different Days", days_diff, 9);
+  }
+
+  @Test
+  public void testSameMonthSameDay() {
+    int days_diff = Cal.cal(1, 8, 1, 8, 2018);
+    assertEquals("Same Month Same Day", days_diff, 0);
+  }
+
+  @Test
+  public void testFebMarchLeapYear() {
+    int days_diff = Cal.cal(2, 20, 3, 1, 2016);
+    assertEquals("Check End of Feb Leap Year (2016)", days_diff, 10);
+  }
+
+  @Test
+  public void testFebMarchLeapYear2000() {
+    int days_diff = Cal.cal(2, 20, 3, 1, 2000);
+    assertEquals("Check End of Feb Leap Year (2000)", days_diff, 10);
+  }
+
+  @Test
+  public void testFebMarchNotLeapYear() {
+    int days_diff = Cal.cal(2, 20, 3, 1, 2018);
+    assertEquals("Check End of Feb NOT Leap Year (2018)", days_diff, 9);
+  }
+
+  @Test
+  public void testFebMarchNotLeapYear2100() {
+    int days_diff = Cal.cal(2, 20, 3, 1, 2017);
+    assertEquals("Check End of Feb NOT Leap Year (2100)", days_diff, 9);
+  }
 }
