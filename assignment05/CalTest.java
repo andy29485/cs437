690bdc19ed186237c7998c956d9db369e66bbb35
Andriy Zasypkin <AndriyZasypkin@gmail.com>
2018-02-22 16:53:44 -0500

    added test class

---
+++ assignment05/CalTest.java
@@ -0,0 +1,16 @@
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
+public class CalTest {
+
+  public static void main(String args[]){
+    org.junit.runner.JUnitCore.main("CalTest");
+  }
+
+  @Before
+  public void setup() {
+    // TODO
+  }
+}
